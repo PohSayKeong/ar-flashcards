5ab5476841b73376785422a1648e21fb7c920a90
PohSayKeong <pohsaykeong@gmail.com>
2024-01-20 15:02:23 +0800

    handle inputs

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,19 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import { ARCanvas } from "@artcom/react-three-arjs"
 import { App } from "./App"
 import "./styles.css"
 
-createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById("root")).render(
+  <ARCanvas
+    gl={{ antialias: false, powerPreference: "default", physicallyCorrectLights: true }}
+    onCameraStreamReady={() => console.log("Camera stream ready")}
+    onCameraStreamError={() => console.error("Camera stream error")}
+    onCreated={({ gl }) => {
+      gl.setSize(window.innerWidth, window.innerHeight)
+    }}>
+    <ambientLight />
+    <pointLight position={[10, 10, 0]} intensity={10.0} />
+    <App />
+  </ARCanvas>,
+)
